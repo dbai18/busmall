86c81cff96c712182b4edb85846be88919b899b6
dbai18 <dbai18@gmail.com>
2017-02-26 10:25:06 -0800

    rewrote as an object for rendering & counting

--- app.js
+++ app.js
@@ -21,9 +21,6 @@ function ProductObject(name, location){
 })();
 // console.log(productObjectsArray);
 
-function randomObject(){
-    return Math.floor(Math.random() * productNamesArray.length);
-}
 
 //wanted to be sure 0 & 19 were returned
 // (function testRandom(){
@@ -31,21 +28,44 @@ function randomObject(){
 //     console.log(randomObject());
 //   }
 // })();
-var randomIndex = [];
-function randomIndexGen(){
-  while (randomIndex.length < 3) {
-        var index = randomObject();
-        if (randomIndex.indexOf(index) === -1) {
-          randomIndex.push(index);
-        }
-      }
-      return randomIndex;
-}
-randomIndexGen();
-function showImages(){
 
-  document.getElementById('left_img').src = productObjectsArray[randomIndex[0]].location;
-  document.getElementById('mid_img').src = productObjectsArray[randomIndex[1]].location;
-  document.getElementById('right_img').src = productObjectsArray[randomIndex[2]].location;
+var renderCounter = {
+  left: null,
+  mid: null,
+  right: null,
+
+  left: document.getElementById('left_img'),
+  mid: document.getElementById('mid_img'),
+  right: document.getElementById('right_img'),
+
+  randomObject: function() {
+    return Math.floor(Math.random() * productNamesArray.length);
+  },
+  showImages: function(){
+    renderCounter.left = productObjectsArray[renderCounter.randomObject()];
+    renderCounter.mid = productObjectsArray[renderCounter.randomObject()];
+    renderCounter.right = productObjectsArray[renderCounter.randomObject()];
+
+    if(renderCounter.left == renderCounter.mid || renderCounter.left == renderCounter.right || renderCounter.mid == renderCounter.right){
+      renderCounter.randomObject();
+    }
+    document.getElementById('left_img').src = renderCounter.left.location;
+    document.getElementById('left_img').id = renderCounter.left.name;
+    document.getElementById('mid_img').src = renderCounter.mid.location;
+    document.getElementById('mid_img').id = renderCounter.mid.name;
+    document.getElementById('right_img').src = renderCounter.right.location;
+    document.getElementById('right_img').id = renderCounter.right.name;
+  }
+
 }
-showImages();
+// var randomIndex = [];
+// function randomIndexGen(){
+//   while (randomIndex.length < 3) {
+//         var index = randomObject();
+//         if (randomIndex.indexOf(index) === -1) {
+//           randomIndex.push(index);
+//         }
+//       }
+//       return randomIndex;
+// }
+// randomIndexGen();
